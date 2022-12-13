d934ff036793096ed79fda5aa37e6f91d0ab48a6
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2022-12-13 17:43:26 +0100

    supply chain

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,10 +1,24 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+//
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraKey = fs.readFileSync(".infuraKey").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*", // Match any network id
-    }
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/${infuraKey}`),
+      network_id: 5,       // goerli's id
+      gas: 5500000,        // goerli has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
   compilers: {
     solc: {
